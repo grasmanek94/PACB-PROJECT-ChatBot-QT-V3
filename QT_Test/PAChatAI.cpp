95efe27713ee50f4c100dc73c5f5f82e94d9198b
Gamer_Z <grasmanek94@gmail.com>
2016-05-21 16:15:10 +0200

    add another answer to location

--- QT_Test/PAChatAI.cpp
+++ QT_Test/PAChatAI.cpp
@@ -184,7 +184,7 @@ std::vector<qp> questions_answers =
 		QStringList{ "", " :)", "", " :P", "", " ;)", "", " :D", "", " ;D", "", " ;P", "" },
 		[&](QString answer)
 		{
-			const static QStringList possible_answers = { "graag", "grag", "ja", "yes", "ok", "nee", "nah", "niet", "mis", "may", "mab", "jup", "mwah", "waarom", "mis", "mischien", "hoezo", "nooit", "altijd", "hangt", "te ", "zou", "ver", "kan", "kun", "helaas", "prim", "top", "goed", "luk" };
+			const static QStringList possible_answers = { "kom", "graag", "grag", "ja", "yes", "ok", "nee", "nah", "niet", "mis", "may", "mab", "jup", "mwah", "waarom", "mis", "mischien", "hoezo", "nooit", "altijd", "hangt", "te ", "zou", "ver", "kan", "kun", "helaas", "prim", "top", "goed", "luk" };
 			for (auto& possible_answer : possible_answers)
 			{
 				if (answer.indexOf(possible_answer) != -1)
